eb7ff4bb0aec1ad1e60dc1b0af48164cd2e5f6d1
venom_play <idaniil535@gmail.com>
2024-12-21 14:43:23 +0300

    upd

---
+++ app/src/main/java/com/example/danmuse/components/app/songPlayer/SongPlayerComponent.kt
@@ -0,0 +1,11 @@
+package com.example.danmuse.components.app.songPlayer
+
+import com.arkivanov.decompose.value.Value
+import com.example.danmuse.media.controller.SongState
+import com.example.danmuse.mvi.app.songPlayer.SongPlayerIntent
+
+interface SongPlayerComponent {
+    val state: Value<SongState>
+
+    fun processIntent(intent: SongPlayerIntent)
+}
